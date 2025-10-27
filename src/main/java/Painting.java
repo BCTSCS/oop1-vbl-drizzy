4e47bb9c1367ec1b4139309e91c7ba548eb171ff
kngo2026 <148019881+kngo2026@users.noreply.github.com>
2025-10-27 14:01:03 -0400

    Painting commit

---
+++ src/main/java/Painting.java
@@ -0,0 +1,14 @@
+public class Painting {
+    private String title;
+    private int year;
+    public Painting(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getYear() {
+        return year;
+    }
+}
