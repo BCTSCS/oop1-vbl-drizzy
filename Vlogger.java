6b7b658d651d1d2b77631d1601ef91eba2219065
Jayden Argudo <jayarg26@bergen.org>
2025-10-27 14:04:16 -0400

    Jayden Vlogger

---
+++ Vlogger.java
@@ -0,0 +1,14 @@
+public class Vlogger {
+    private String name;
+    private int yearJoined;
+    public Vlogger(){
+        this.name = "jayden";
+        this.yearJoined = 67;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getYearJoined(){
+        return yearJoined;
+    }
+}
