38f15ed94f84e9e0856cf12b3379e28e355d8da7
Jayden Argudo <jayarg26@bergen.org>
2025-10-27 14:27:50 -0400

    Daniel

--- src/main/java/UserApp.java
+++ src/main/java/UserApp.java
@@ -1,1 +1,15 @@
-
+public class UserApp extends JFrame(){
+    private JPanel panel;
+    private Painting painting;
+    private Vlogger vlogger;
+    private Player user;
+    public UserApp(){
+        panel = new JPanel();
+        painting = new Painting();
+        vlogger = new Vlogger();
+        user = new Player();
+    }
+    public void main(String[] args){
+        new UserApp();
+    }
+}
